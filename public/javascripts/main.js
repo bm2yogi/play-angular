400cd3d04eff866d35cc77c3727394a3000ba684
Michael Ibarra <bm2yogi@gmail.com>
2014-11-07 14:35:59 -0800

    added page2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,5 +17,58 @@
 		};
 	})
 
-	
+	main.controller('NFCWestController', function($scope){
+		$scope.conference="NFC West",
+		$scope.teams=[
+			{name:'Seahawks',city:'Seattle'},
+			{name:'Cardinals',city:'Arizona'},
+			{name:'49ers',city:'San Francisco'},
+			{name:'Rams',city:'St. Louis'}
+		];
+	});
+
+	main.directive('nfcSouthDirective', function($scope){
+		var conf = $scope;
+		return{
+			restrict:"E",
+			templateUrl:"team-list.html",
+			controller: function(conf){
+				conf.conference="NFC South",
+				conf.teams=[
+					{name:"Falcons",city:"Atlanta"},
+					{name:"Panthers",city:"Carolina"},
+					{name:"Bucaneers",city:"Tampa Bay"},
+					{name:"Saints",city:"New Orleans"}
+				];
+			}
+		};
+	});
+
+	main.controller('NFCEastController', function(){
+		this.conference="NFC East",
+		this.teams=[
+			{name:"Eagles",city:"Philadelphia"},
+			{name:"Cowboys",city:"Dallas"},
+			{name:"Giants",city:"New York"},
+			{name:"Redskins",city:"Washington"}
+		];
+	});
+
+	main.directive('nfcNorthDirective', function(){
+		return{
+			restrict:"E",
+			templateUrl:"team-list.html",
+			controllerAs: "conf",
+			controller: function(){
+				this.conference="NFC North",
+				this.teams=[
+					{name:"Lions",city:"Detroit"},
+					{name:"Vikings",city:"Minnesota"},
+					{name:"Bears",city:"Chicago"},
+					{name:"Packers",city:"Green Bay"}
+				];
+			}
+		};
+	});
+
 })();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Angular Demo w/ Express' });
 });
 
+router.get('/page2', function(req, res) {
+  res.render('page2', { title: 'Teams' });
+});
+
 module.exports = router;
